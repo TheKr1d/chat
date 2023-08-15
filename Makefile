93234f791ca721d12633c428a4c0d32b184e70d2
TheKr1d <epifanov.vv.spb@gmail.com>
2023-08-15 20:07:49 +0300

    add: router

---
+++ Makefile
@@ -0,0 +1,3 @@
+start:
+	npm start
+	npx start-server
---
+++ src/routes/root.jsx
@@ -0,0 +1,10 @@
+import '../css/root.css';
+
+export default function Root() {
+    return (
+        <>
+            <div>Root</div>
+            <a href={`/login`}>Your Name</a>
+        </>
+    )
+}
