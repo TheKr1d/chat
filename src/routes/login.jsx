2fb41f8ea6c4656cc8bfd2cbe3c772d3a249ca19
TheKr1d <epifanov.vv.spb@gmail.com>
2023-08-16 20:10:48 +0300

    add login form

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,7 +1,42 @@
+import "../css/login.css";
+import { useFormik } from 'formik';
+
 export default function Login() {
+    const initialValues = {
+        login: '',
+        password: ''
+    };
+    const formik = useFormik({
+        initialValues,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+
+    //alert(JSON.stringify(values, null, 2));
     return (
-        <>
-            <div id="login">Login</div>
-        </>
+        <div className="container">
+            <form onSubmit={formik.handleSubmit} className="form">
+                <label htmlFor="login">Login</label>
+                <input
+                    id="login"
+                    name="login"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.login}
+                />
+                <label htmlFor="login">Password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                />
+                <button type="submit">Submit</button>
+
+            </form>
+        </div>
     )
 }
